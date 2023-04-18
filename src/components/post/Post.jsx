3a692066b299b840c0a05469f28928d9fa59d894
Marvinmartain <122340360+Marvinmartain@users.noreply.github.com>
2023-04-18 12:40:27 -0400

    errorfix

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -11,7 +11,14 @@ export default function Post() {
       <div className="postCats">
         <span className="postCat">Music</span>
         <span className="postCat">Life</span>
-      </div>  
+    </div>
+
+    <span className="postTitle">
+     Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    </span>
+    
+    <hr/>
+    <span className="postDate">1 hour ago</span>
     </div>
 
     </div>
