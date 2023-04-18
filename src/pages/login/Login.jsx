56ec7ecf6424474bfb2595adad85c57c399eeda5
Marvinmartain <122340360+Marvinmartain@users.noreply.github.com>
2023-04-18 16:41:43 -0400

    login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,12 +3,13 @@ import "./login.css"
 export default function Login() {
   return (
 <div className="login">
+    <span className="loginTitle">Login</span>
     <form className="loginform">
       <label>Email</label>
-      <input type="text" placeholder="Enter Your Email..."/>
+      <input type="text" className="loginInput" placeholder="Enter Your Email..."/>
 
       <label>Password</label>
-      <input type="password" placeholder="Enter Your Password..."/>
+      <input type="password" className="loginPassword" placeholder="Enter Your Password..."/>
 
       <button className="loginButton">Login</button>
       </form>
