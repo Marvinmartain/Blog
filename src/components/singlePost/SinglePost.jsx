9812a4b5447a3c46edfa2823ffdbb56664f3a4e7
Marvinmartain <122340360+Marvinmartain@users.noreply.github.com>
2023-04-18 14:19:10 -0400

    webworking

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -3,7 +3,26 @@ import "./singlePost.css"
 export default function SinglePost() {
   return (
 <div className="singlePost">
-    single Post
+  <div className="singlePostWrapper">
+
+<img src="https://images.wombo.art/generated/d50de073-57ce-4c6e-8322-7ea5dd62f199/final.jpg?Expires=1689040619&Signature=R2KmOgn1xiiU3a~FlYZfISx~fh3~vpxffgtLLLCkkHOvRt1CssbUV45pu5KI7oPuXeItcqNyvWjSjC0Oe17CnLestAM18QpLoWRdyGBFGz20efhHFmQDi7mG61bfNzTroqYfEf1uWZg-Ko8zBCQP-g3wKO8sLTQ916fEWkytzHLc2YiF9Ja8FlOCqARbBHrAmgGfRLpMXxhtpt-NfniLqyGC-qXE4YadgRqpGpoIJuIyA19j7vvLW26qTlDu60ku0nZKyJNdI6ebTYPrHw6jgvXNCVCT1PvWiqRIFQ~5SBMsbPMPi6RwuWl6Kr9Ko8ukSkf7boFKZ7fP-1rciSWaCQ__&Key-Pair-Id=K1ZXCNMC55M2IL" alt="" className="singlePostImg"
+/>
+
+<h1 className="singlePostTitle">
+ Lorem ipsum, dolor sit amet?
+ <div className="singlePostEdit">
+ <i className="singlePostIcon far fa-edit"></i>
+ <i class="singlePostIcon far fa-trash-alt"></i>
+ </div>
+</h1>
+
+<div className="singlePostInfo">
+  <span className="singlePostAuthor">Autor: <b>Human</b></span>
+  <span className="singlePostDate">1 hour ago 04/18/2023</span>
+</div>
+
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, necessitatibus doloremque tenetur, hic eum nihil cum esse quasi at iusto explicabo dolorem mollitia voluptate aperiam corporis sint eos modi eveniet.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, necessitatibus doloremque tenetur, hic eum nihil cum esse quasi at iusto explicabo dolorem mollitia voluptate aperiam corporis sint eos modi eveniet.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, necessitatibus doloremque tenetur, hic eum nihil cum esse quasi at iusto explicabo dolorem mollitia voluptate aperiam corporis sint eos modi eveniet.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti, necessitatibus doloremque tenetur, hic eum nihil cum esse quasi at iusto explicabo dolorem mollitia voluptate aperiam corporis sint eos modi eveniet.</p>
+</div>
 </div>
   )
 }
