20f5cfebd8d0c9016712de76e7c4505873d31295
Marvinmartain <122340360+Marvinmartain@users.noreply.github.com>
2023-04-18 16:52:08 -0400

    Register

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import Login from "./pages/login/Login"
+import Register from "./pages/register/Register";
 
 function App() {
   return (
     <div>
     <TopBar />
-    <Login />
+    <Register />
    
   </div>
   );
